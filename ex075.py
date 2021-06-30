79130cf468d4b0355a23ef21570f8308a6c1cd48
marcos-mpc <marcospereirachaves17@gmail.com>
2021-06-30 15:58:20 -0300

    MUNDO 3

--- ex075.py
+++ ex075.py
@@ -6,7 +6,7 @@ if 3 in n:
     print(f'o numero 3 aparece na {n.index(3) + 1}ª posição')
 else:
     print('não foi digitado nenhum numero 3')
-print('valor par digitado foi: ',end='')
+print('valor par digitado foi: ', end='')
 for num in n:
     cont = 0
     if num % 2 == 0:
