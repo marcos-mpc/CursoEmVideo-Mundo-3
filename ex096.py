ba44e89daaaf35a075c21de9ced597acd98e3a21
marcos-mpc <marcospereirachaves17@gmail.com>
2021-07-21 15:01:22 -0300

    MUNDO 3

--- ex096.py
+++ ex096.py
@@ -1,7 +1,10 @@
-def aria(a, b):
+print(f'{20*"="}\n{"ÁREA DE TERRENO"}\n{20*"="}')
+
+
+def area(a, b):
     print(30*'=')
-    print(f'com a largura de {a:.2f}m e comprimento de {b:.2f}m temos uma ária de {a * b:.2f}m²')
+    print(f'com a largura de {a:.2f}m e comprimento de {b:.2f}m temos uma área de {a * b:.2f}m²')
     print(30*'=')
 
 
-aria(a=float(input('Largura (m): ')), b=float(input('Comprimento (m): ')))
+area(a=float(input('Largura (m): ')), b=float(input('Comprimento (m): ')))
