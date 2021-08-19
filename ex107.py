1b1f33987b3bd1eca52cdc72f9d828f40a46fe10
marcos-mpc <marcospereirachaves17@gmail.com>
2021-08-19 20:54:31 -0300

    MUNDO 3

---
+++ ex107.py
@@ -0,0 +1,7 @@
+import funcoes107
+
+n = float(input('Digite um valor: '))
+print(f'A metade de {n} é igual à {funcoes107.metade(n)}')
+print(f'O dobro de {n} é igual à {funcoes107.dobro(n)}')
+print(f'Com aumento de 10% fica {funcoes107.aumentar(n, 10)}')
+print(f'Com redução de 13% fica {funcoes107.diminuir(n, 13)}')
