93870576afaf7427f6b3e0845967c2f03c2e514b
marcos-mpc <marcospereirachaves17@gmail.com>
2021-08-19 21:09:53 -0300

    MUNDO 3

--- ex107.py
+++ ex107.py
@@ -1,7 +1,7 @@
-import moeda
+import moeda107
 
 n = float(input('Digite um valor: '))
-print(f'A metade de {n} é igual à {moeda.metade(n)}')
-print(f'O dobro de {n} é igual à {moeda.dobro(n)}')
-print(f'Com aumento de 10% fica {moeda.aumentar(n, 10)}')
-print(f'Com redução de 13% fica {moeda.diminuir(n, 13)}')
+print(f'A metade de {n} é igual à R${moeda107.metade(n)}')
+print(f'O dobro de {n} é igual à R${moeda107.dobro(n)}')
+print(f'Com aumento de 10% fica R${moeda107.aumentar(n, 10)}')
+print(f'Com redução de 13% fica R${moeda107.diminuir(n, 13)}')
